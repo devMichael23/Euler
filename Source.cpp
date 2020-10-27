55400cf94a4105d73edd7203bfb74e660e282799
Misha Petrushinin <petrushinin.mikhail@outlook.com>
2020-10-27 19:25:46 +0300

    Коммит для отчета

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <omp.h>
+#include <time.h>
 #include <cmath>
 
 using namespace std;
@@ -18,18 +19,23 @@ double euler(long long n)
 
 int main()
 {
-    long long n = pow(10, 10);
+    long long n = pow(10, 15);
     double result = 0;
-
+    clock_t start = clock();
     omp_set_num_threads(8);
 #pragma omp parallel for
     for (long long i = 0; i < n; i++)
     {
         result += 1 / (pow(i+1, 2));
     }
+
+    clock_t end = clock();
+    double sec = (double)(end - start);
+
     result = sqrt(result * 6);
 
     cout << result << endl;
+    cout << sec << " sec" << endl;
 
     system("pause");
     return 0;
