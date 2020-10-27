3b66812f267903fcfdffb01a726f5212a65b22b7
Misha Petrushinin <petrushinin.mikhail@outlook.com>
2020-10-27 14:44:31 +0300

    Финальный коммит

--- Source.cpp
+++ Source.cpp
@@ -1,14 +1,36 @@
 #include <iostream>
 #include <omp.h>
+#include <cmath>
 
 using namespace std;
 
+double euler(long long n)
+{
+    double result = 0;
+
+    for (long long i = 0; i < n; i++)
+    {
+        result += 1/(pow(i+1, 2));
+    }
+    result = sqrt(result*6);
+    return result;
+}
+
 int main()
 {
-#pragma omp parallel
-    {
-        cout << "Hello World\n";
-    }
+    long long n = pow(10, 10);
+    double result = 0;
 
+    omp_set_num_threads(8);
+#pragma omp parallel for
+    for (long long i = 0; i < n; i++)
+    {
+        result += 1 / (pow(i+1, 2));
+    }
+    result = sqrt(result * 6);
+
+    cout << result << endl;
+
+    system("pause");
     return 0;
 }
