2c7b1ce7afa90945a773629a0a724a3c6fef6dba
Misha Petrushinin <petrushinin.mikhail@outlook.com>
2020-10-28 20:12:10 +0300

    Улучшения кода

--- Source.cpp
+++ Source.cpp
@@ -5,310 +5,159 @@
 
 using namespace std;
 
-double euler(long long n)
+void euler(long long n, int threads)
 {
     double result = 0;
-
-    for (long long i = 0; i < n; i++)
+#pragma omp parallel num_threads(threads)
     {
-        result += 1 /(pow(i+1, 2));
-    }
-    result = sqrt(result*6);
-    return result;
-}
+        clock_t start = clock();
 
-double euler(long long n, int threads)
-{
-    double result = 0;
-    omp_set_num_threads(threads);
-#pragma omp parallel for
-    for (long long i = 0; i < n; i++)
-    {
-        result += 1 /(pow(i+1,2));
-    }
+    #pragma omp parallel for
+        for (long long i = 0; i < n; i++)
+        {
+            result += 1 / (pow(i + 1, 2));
+        }
 
-    result = sqrt(result * 6);
-    return result;
+        result = sqrt(result * 6);
+        clock_t end = clock();
+        double sec = (double)(end - start) / CLOCKS_PER_SEC;
+        cout << "result = " << result << "; " << sec << " sec; threads = " << threads << "; n = 10^10" << endl;
+    }
+    
 }
 
 int main()
 {
-    // 4 8 16 32 64
 #pragma region n = 10^10
     int threads = 1;
-    long long n = pow(10, 6);
-    double result = 0;
-    clock_t start = clock();
-    result = euler(n);
-    clock_t end = clock();
-    double sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^10\n", result, sec, threads);
+    long long n = pow(10, 10);
+    euler(n, threads);
 
     threads = 4;
-    start = clock();
-    result = euler(n, threads);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^10\n", result, sec, threads);
+    euler(n, threads);
 
     threads = 8;
-    start = clock();
-    result = euler(n, threads);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^10\n", result, sec, threads);
+    euler(n, threads);
 
     threads = 16;
-    start = clock();
-    result = euler(n, threads);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^10\n", result, sec, threads);
+    euler(n, threads);
 
     threads = 32;
-    start = clock();
-    result = euler(n, threads);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^10\n", result, sec, threads);
+    euler(n, threads);
 
     threads = 64;
-    start = clock();
-    result = euler(n, threads);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^10\n", result, sec, threads);
+    euler(n, threads);
 #pragma endregion
 
 #pragma region n = 10^11
-    n = pow(10, 7);
+    n = pow(10, 11);
 
     threads = 1;
-    start = clock();
-    result = euler(n);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^11\n", result, sec, threads);
+    euler(n, threads);
 
     threads = 4;
-    start = clock();
-    result = euler(n, threads);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^11\n", result, sec, threads);
+    euler(n, threads);
 
     threads = 8;
-    start = clock();
-    result = euler(n, threads);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^11\n", result, sec, threads);
+    euler(n, threads);
 
     threads = 16;
-    start = clock();
-    result = euler(n, threads);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^11\n", result, sec, threads);
+    euler(n, threads);
 
     threads = 32;
-    start = clock();
-    result = euler(n, threads);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^11\n", result, sec, threads);
+    euler(n, threads);
 
     threads = 64;
-    start = clock();
-    result = euler(n, threads);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^11\n", result, sec, threads);
+    euler(n, threads);
 #pragma endregion
 
 #pragma region n = 10^12
     n = pow(10, 12);
 
     threads = 1;
-    start = clock();
-    result = euler(n);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^12\n", result, sec, threads);
+    euler(n, threads);
 
     threads = 4;
-    start = clock();
-    result = euler(n, threads);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^12\n", result, sec, threads);
+    euler(n, threads);
 
     threads = 8;
-    start = clock();
-    result = euler(n, threads);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^12\n", result, sec, threads);
+    euler(n, threads);
 
     threads = 16;
-    start = clock();
-    result = euler(n, threads);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^12\n", result, sec, threads);
+    euler(n, threads);
 
     threads = 32;
-    start = clock();
-    result = euler(n, threads);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^12\n", result, sec, threads);
+    euler(n, threads);
 
     threads = 64;
-    start = clock();
-    result = euler(n, threads);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^12\n", result, sec, threads);
+    euler(n, threads);
 #pragma endregion
 
 #pragma region n = 10^13
     n = pow(10, 13);
 
     threads = 1;
-    start = clock();
-    result = euler(n);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^13\n", result, sec, threads);
+    euler(n, threads);
 
     threads = 4;
-    start = clock();
-    result = euler(n, threads);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^13\n", result, sec, threads);
+    euler(n, threads);
 
     threads = 8;
-    start = clock();
-    result = euler(n, threads);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^13\n", result, sec, threads);
+    euler(n, threads);
 
     threads = 16;
-    start = clock();
-    result = euler(n, threads);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^13\n", result, sec, threads);
+    euler(n, threads);
 
     threads = 32;
-    start = clock();
-    result = euler(n, threads);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^13\n", result, sec, threads);
+    euler(n, threads);
 
     threads = 64;
-    start = clock();
-    result = euler(n, threads);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^13\n", result, sec, threads);
+    euler(n, threads);
 #pragma endregion
 
 #pragma region n = 10^14
     n = pow(10, 14);
 
     threads = 1;
-    start = clock();
-    result = euler(n);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^14\n", result, sec, threads);
-
+    euler(n, threads);
+ 
     threads = 4;
-    start = clock();
-    result = euler(n, threads);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^14\n", result, sec, threads);
-
+    euler(n, threads);
+ 
     threads = 8;
-    start = clock();
-    result = euler(n, threads);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^14\n", result, sec, threads);
-
+    euler(n, threads);
+ 
     threads = 16;
-    start = clock();
-    result = euler(n, threads);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^14\n", result, sec, threads);
-
+    euler(n, threads);
+ 
     threads = 32;
-    start = clock();
-    result = euler(n, threads);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^14\n", result, sec, threads);
-
+    euler(n, threads);
+ 
     threads = 64;
-    start = clock();
-    result = euler(n, threads);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^14\n", result, sec, threads);
-#pragma endregion
+    euler(n, threads);
+ #pragma endregion
 
 #pragma region n = 10^15
     n = pow(10, 15);
 
     threads = 1;
-    start = clock();
-    result = euler(n);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^15\n", result, sec, threads);
+    euler(n, threads);
 
     threads = 4;
-    start = clock();
-    result = euler(n, threads);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^15\n", result, sec, threads);
-
+    euler(n, threads);
+ 
     threads = 8;
-    start = clock();
-    result = euler(n, threads);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^15\n", result, sec, threads);
-
+    euler(n, threads);
+ 
     threads = 16;
-    start = clock();
-    result = euler(n, threads);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^15\n", result, sec, threads);
-
+    euler(n, threads);
+ 
     threads = 32;
-    start = clock();
-    result = euler(n, threads);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^15\n", result, sec, threads);
-
+    euler(n, threads);
+ 
     threads = 64;
-    start = clock();
-    result = euler(n, threads);
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("result = %f; %f sec; thread = %d; n = 10^15\n", result, sec, threads);
-#pragma endregion
+    euler(n, threads);
+ #pragma endregion
 
 
     system("pause");
