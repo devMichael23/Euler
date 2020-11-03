70fc4253e18a98635faee5509e47f7771406cf09
Misha Petrushinin <petrushinin.mikhail@icloud.com>
2020-11-03 23:10:49 +0300

    Последний коммит для сдачи практической работы

--- Source.cpp
+++ Source.cpp
@@ -1,165 +1,99 @@
-#include <iostream>
+﻿#include <iostream>
 #include <omp.h>
 #include <time.h>
 #include <cmath>
 
 using namespace std;
 
-void euler(long long n, int threads)
+
+// Параллельное вычисление числа π, используя ряд Эйлера
+double euler(long long n, int threads)
 {
     double result = 0;
-#pragma omp parallel num_threads(threads)
+    omp_set_num_threads(threads);
+#pragma omp parallel
     {
-        clock_t start = clock();
+        result = 0;
 
-    #pragma omp parallel for
+#pragma omp parallel for
         for (long long i = 0; i < n; i++)
         {
             result += 1 / (pow(i + 1, 2));
         }
 
         result = sqrt(result * 6);
-        clock_t end = clock();
-        double sec = (double)(end - start) / CLOCKS_PER_SEC;
-        cout << "result = " << result << "; " << sec << " sec; threads = " << threads << "; n = 10^10" << endl;
     }
-    
+    return result;
 }
 
 int main()
 {
-#pragma region n = 10^10
+    setlocale(LC_CTYPE, "Russian");
+    cout << "Практическая работа №2" << endl;
+    cout << "Параллельное программирование с использованием технолгии OpenMP" << endl;
+    cout << "Бузыкин Игорь" << endl;
+    cout << endl;
+    cout << "Введите степень числа 10: ";
+    int paw;
+    cin >> paw;
+    double result = 0;
     int threads = 1;
-    long long n = pow(10, 10);
-    euler(n, threads);
+    cout << "Измерение скорости при n = 10^" << paw << endl;
+    long long n = pow(10, paw);
+    clock_t start = clock();
+
+    result = euler(n, threads);
+
+    clock_t end = clock();
+    double sec = (double)(end - start) / CLOCKS_PER_SEC;
+    cout << "result = " << result << "; " << sec << " sec; threads = " << threads << "; n = 10^" << paw << endl;
 
     threads = 4;
-    euler(n, threads);
+    start = clock();
+
+    result = euler(n, threads);
+
+    end = clock();
+    sec = (double)(end - start) / CLOCKS_PER_SEC;
+    cout << "result = " << result << "; " << sec << " sec; threads = " << threads << "; n = 10^" << paw << endl;
 
     threads = 8;
-    euler(n, threads);
+    start = clock();
+
+    result = euler(n, threads);
+
+    end = clock();
+    sec = (double)(end - start) / CLOCKS_PER_SEC;
+    cout << "result = " << result << "; " << sec << " sec; threads = " << threads << "; n = 10^" << paw << endl;
 
     threads = 16;
-    euler(n, threads);
+    start = clock();
+
+    result = euler(n, threads);
+
+    end = clock();
+    sec = (double)(end - start) / CLOCKS_PER_SEC;
+    cout << "result = " << result << "; " << sec << " sec; threads = " << threads << "; n = 10^" << paw << endl;
 
     threads = 32;
-    euler(n, threads);
+    start = clock();
+
+    result = euler(n, threads);
+
+    end = clock();
+    sec = (double)(end - start) / CLOCKS_PER_SEC;
+    cout << "result = " << result << "; " << sec << " sec; threads = " << threads << "; n = 10^" << paw << endl;
 
     threads = 64;
-    euler(n, threads);
-#pragma endregion
+    start = clock();
 
-#pragma region n = 10^11
-    n = pow(10, 11);
+    result = euler(n, threads);
 
-    threads = 1;
-    euler(n, threads);
-
-    threads = 4;
-    euler(n, threads);
-
-    threads = 8;
-    euler(n, threads);
-
-    threads = 16;
-    euler(n, threads);
-
-    threads = 32;
-    euler(n, threads);
-
-    threads = 64;
-    euler(n, threads);
-#pragma endregion
-
-#pragma region n = 10^12
-    n = pow(10, 12);
-
-    threads = 1;
-    euler(n, threads);
-
-    threads = 4;
-    euler(n, threads);
-
-    threads = 8;
-    euler(n, threads);
-
-    threads = 16;
-    euler(n, threads);
-
-    threads = 32;
-    euler(n, threads);
-
-    threads = 64;
-    euler(n, threads);
-#pragma endregion
-
-#pragma region n = 10^13
-    n = pow(10, 13);
-
-    threads = 1;
-    euler(n, threads);
-
-    threads = 4;
-    euler(n, threads);
-
-    threads = 8;
-    euler(n, threads);
-
-    threads = 16;
-    euler(n, threads);
-
-    threads = 32;
-    euler(n, threads);
-
-    threads = 64;
-    euler(n, threads);
-#pragma endregion
-
-#pragma region n = 10^14
-    n = pow(10, 14);
-
-    threads = 1;
-    euler(n, threads);
- 
-    threads = 4;
-    euler(n, threads);
- 
-    threads = 8;
-    euler(n, threads);
- 
-    threads = 16;
-    euler(n, threads);
- 
-    threads = 32;
-    euler(n, threads);
- 
-    threads = 64;
-    euler(n, threads);
- #pragma endregion
-
-#pragma region n = 10^15
-    n = pow(10, 15);
-
-    threads = 1;
-    euler(n, threads);
-
-    threads = 4;
-    euler(n, threads);
- 
-    threads = 8;
-    euler(n, threads);
- 
-    threads = 16;
-    euler(n, threads);
- 
-    threads = 32;
-    euler(n, threads);
- 
-    threads = 64;
-    euler(n, threads);
- #pragma endregion
+    end = clock();
+    sec = (double)(end - start) / CLOCKS_PER_SEC;
+    cout << "result = " << result << "; " << sec << " sec; threads = " << threads << "; n = 10^" << paw << endl;
+    cout << endl;
 
 
-    system("pause");
     return 0;
 }
